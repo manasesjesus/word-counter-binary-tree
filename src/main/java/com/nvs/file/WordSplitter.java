f86a5c8a54da92e1edca417915978dc9db9179a9
Manasés Jesús <contact@manasesjesus.com>
2018-07-24 09:04:52 +0200

    formatting the code

--- src/main/java/com/nvs/file/WordSplitter.java
+++ src/main/java/com/nvs/file/WordSplitter.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -17,8 +18,8 @@ import java.util.Scanner;
  *
  */
 public class WordSplitter {
-	private List<String> ignored_chars = Arrays.asList( "(", ")", ",", ".", ";" );
-	
+	private List<String> ignored_chars = Arrays.asList("(", ")", ",", ".", ";");
+
 	/**
 	 * @return the ignored_chars
 	 */
@@ -29,38 +30,46 @@ public class WordSplitter {
 	/**
 	 * Add one character to the ignored_chars
 	 * 
-	 * @param char the character to be ignored  
+	 * @param char the character to be ignored
 	 */
 	public void setIgnored_chars(String ignored_char) {
 		this.ignored_chars.add(ignored_char);
 	}
 
+	/**
+	 * @param filename the name of the file
+	 * @return a sorted map containing the word counting
+	 * @throws FileNotFoundException
+	 */
 	@SuppressWarnings("resource")
-	public Map<String, Integer> getWordsCountFromFile (String filename) throws FileNotFoundException {		
+	public Map<String, Integer> getWordsCountFromFile(String filename) throws FileNotFoundException {
 		String line;
-        String words = " ";
-        Map<String, Integer> words_count = new HashMap<>();
-        Scanner scanner = new Scanner(new File(filename)).useDelimiter("\n");
+		String words = " ";
+		Map<String, Integer> words_count = new HashMap<>();
+		Scanner scanner = new Scanner(new File(filename)).useDelimiter("\n");
 
-        while (scanner.hasNext()) {
-            line = scanner.next().trim();
-            
-            for (String ic : ignored_chars) {
-            	line = line.replace(ic, " ");
-            }
-            
-            words += line.trim() + " ";
-        }
+		while (scanner.hasNext()) {
+			line = scanner.next().trim();
 
-        if (words != " " ) {
-        	Arrays.asList(words.split(" ")).forEach(
-        			word -> words_count.merge(word, 1, Integer::sum)
-        	);
-        	words_count.remove("");
-        	
-        	return words_count;
-        }
-        
-        return null;
+			for (String ic : ignored_chars) {
+				line = line.replace(ic, " ");
+			}
+
+			words += line.trim() + " ";
+		}
+
+		if (words != " ") {
+			Map<String, Integer> words_sorted = new LinkedHashMap<>();
+
+			Arrays.asList(words.split(" ")).forEach(word -> words_count.merge(word, 1, Integer::sum));
+
+			words_count.remove("");
+			words_count.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue())
+					.forEachOrdered(entry -> words_sorted.put(entry.getKey(), entry.getValue()));
+
+			return words_sorted;
+		}
+
+		return null;
 	}
 }
