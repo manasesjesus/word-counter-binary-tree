f398677e1a92e810b8a33b826af6893ff1f00d74
Manasés Jesús <contact@manasesjesus.com>
2018-07-25 13:54:48 +0200

    adding the tree classes

---
+++ src/main/java/com/nvs/tree/MagicTree.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package com.nvs.tree;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * This class contains two attributes: the words counting map to be used to build 
+ * the tree and a node to identify the root of the tree. The map must be passed when 
+ * instantiating a new magic object and the tree is built afterwards.
+ * 
+ * The toString method is overridden, so a magic tree object can be printed using the System.out. 
+ * 
+ * @author Manasés Jesús
+ * @github manasesjesus
+ *
+ */
+public class MagicTree {
+	private Map<String, Integer> words_count = null;
+	private Node root = null;
+	
+	/**
+	 * Creates a new magic tree based on the received map 
+	 * 
+	 * @param words_count
+	 */
+	public MagicTree (Map<String, Integer> words_count) {
+		this.words_count = words_count;
+		buildTree();
+	}
+	
+	/**
+	 * Where the magic happens
+	 */
+	private void buildTree () {
+		// There must be at least two words, otherwise it isn't a tree rather 
+		// just the seed about to germinate ó.ó
+		if (words_count != null && words_count.size() >= 2) {
+			// Find the pivot  
+			int size   = words_count.size();
+			int middle = size % 2 == 0 ? size / 2 : size / 2 + 1;
+			
+			ArrayList<String> words = new ArrayList<String>(words_count.keySet());
+			
+			Node childL  = null;
+			Node childR  = null;
+			Node parentL = null;
+			Node parentR = null;
+			
+			// Build the left side of the tree
+			parentL = new Node(words_count.get(words.get(0)), words.get(0));
+			for (int i=1; i<middle; i++) {
+				childR = parentL;
+				childL = new Node(words_count.get(words.get(i)), words.get(i));
+				parentL = new Node(sumNodesWeight(childL, childR));
+				parentL.setLeft(childL);
+				parentL.setRight(childR);
+			}
+			
+			// Build the right side of the tree
+			parentR = new Node(words_count.get(words.get(size - 1)), words.get(size - 1));
+			for (int i=size-2; i>=middle; i--) {
+				childR = parentR;
+				childL = new Node(words_count.get(words.get(i)), words.get(i));
+				parentR = new Node(sumNodesWeight(childL, childR));
+				parentR.setLeft(childL);
+				parentR.setRight(childR);
+			}
+		
+			// I am Root
+			root = new Node(sumNodesWeight(parentL, parentR)); 
+			root.setLeft(parentL);
+			root.setRight(parentR);
+		} 
+		else {
+			System.err.println("Not enough words were found in the file");
+		}
+	}
+
+	/**
+	 * Returns the sum of two nodes' weights 
+	 * 
+	 * @param n1 The first node
+	 * @param n2 The second node
+	 * @return The sum of the nodes' weights
+	 */
+	private int sumNodesWeight (Node n1, Node n2) {
+		return n1.getWeight() + n2.getWeight();
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString () {
+		if (root != null) {
+			root.print();
+		}
+		
+		return "";
+	}
+}
---
+++ src/main/java/com/nvs/tree/Node.java
@@ -0,0 +1,109 @@
+/**
+ * 
+ */
+package com.nvs.tree;
+
+/**
+ * The tree is built using nodes. Each node can be a parent having two children represented by left/right nodes.
+ * Each node has a weight which is the number of occurrences of a word or the sum of two leaves' weights.
+ * A leaf node contains the word being counted.
+ * 
+ * Simple implementation not using inheritance.
+ * 
+ * @author Manasés Jesús
+ * @github manasesjesus
+ *
+ */
+public class Node {
+	private String word;
+	private int weight;
+	private Node left;
+	private Node right;
+
+	/**
+	 * Create a tree node 
+	 * 
+	 * @param weight The sum of two leaves' weights
+	 */
+	public Node (int weight) {
+		this.weight = weight;
+		word  = null;
+		right = null;
+		left  = null;
+	}
+	
+	/**
+	 * Create a leaf node 
+	 * 
+	 * @param weight The number of occurrences
+	 * @param word   The word being counted
+	 */
+	public Node (int weight, String word) {
+		this(weight);
+		this.word = word;
+	}
+
+	/**
+	 * General public method to print the tree. It calls the private method taking the current node as the root.
+	 */
+	public void print () {
+        print("", this, false, true);
+    }
+
+    /**
+     * A recursive method to print the tree to the system's default output
+     * 
+     * @param branch The branches to outline the tree structure
+     * @param n		 The current node
+     * @param isLeft A flag to identify if the current node is a left child
+     * @param isRoot A flag to identify if the current node is the root
+     */
+    private void print (String branch, Node n, boolean isLeft, boolean isRoot) {
+        if (n != null) {
+        	String leaf = n.word == null ? "" : "  [ " + n.word + " ]";
+        	String bstr = branch + (isLeft ? "│  " : "   "); 
+        	
+            System.out.println(branch + (isLeft ? "├──" : isRoot ? "   " : "└––") + n.getWeight() + leaf);
+            print(bstr, n.getLeft(), true, false);
+            print(bstr, n.getRight(), false, false);
+        }
+    }
+	
+    /* Getters & Setters */
+    
+	/**
+	 * @return the left
+	 */
+	public Node getLeft() {
+		return left;
+	}
+
+	/**
+	 * @param left the left to set
+	 */
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	/**
+	 * @return the right
+	 */
+	public Node getRight() {
+		return right;
+	}
+
+	/**
+	 * @param right the right to set
+	 */
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+	/**
+	 * @return the weight
+	 */
+	public int getWeight() {
+		return weight;
+	}
+	
+}
